6307b45415f9874af0a3c6e766b914fec01dfe42
SebasAlvarado420 <drsebastianalvarado88@gmail.com>
2024-02-28 23:47:42 -0600

    Funcion de resta agregada

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 def suma(num_1 : int, num_2 : int) -> int:
     return num_1 + num_2
 
+def resta(num_1 : int, num_2 : int) -> int:
+    return num_1 - num_2
+
+
+
 if __name__ == '__main__':
     print(suma(num_1=2, num_2=4))
+    print(resta(num_1=2, num_2=5))
