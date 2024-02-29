c0c5296ddd5905cf43bbf668b6f6b24139cfec0b
Tanisha Miranda <tanishamiranda@MacBook-Air-77.local>
2024-02-29 12:12:00 -0600

    Funcion de gato

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ def resta(num_1 : int, num_2 : int) -> int:
 
 
 
+def gato(animal: str):
+    if animal == 'cat':
+        return True
+    else:
+        return False
+
+
 if __name__ == '__main__':
     print(suma(num_1=2, num_2=4))
     print(resta(num_1=2, num_2=5))
+    print(gato(animal='cat'))
